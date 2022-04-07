e9fb24d885433f6025c45fe69720abe801dd6a47
pesmarkd <devin.pesmark@gmail.com>
2022-04-07 19:35:11 -0400

    Enemy changes

--- Treasure Hoarder/Assets/Scripts/EnemyMovement.cs
+++ Treasure Hoarder/Assets/Scripts/EnemyMovement.cs
@@ -7,25 +7,36 @@ public class EnemyMovement : MonoBehaviour
 
     public Rigidbody2D rb;
     public Collider2D col;
+    private Collider2D colPlayer;
     public GameObject player;
     public float speed;
 
     private void Start()
     {
         player = GameObject.FindWithTag("Player");
+        colPlayer = player.GetComponent<Collider2D>();
     }
 
     void Update()
     {
         //Debug.Log(gameObject.transform.position);
-        if (col.IsTouching(col))
+        if (col.IsTouching(colPlayer))
         {
-            //rb.AddForce(new Vector2(col.))
+            rb.velocity = new Vector2(0, 0);
         }
+        
     }
 
     private void FixedUpdate()
     {
         gameObject.transform.position = gameObject.transform.position + ((player.transform.position - gameObject.transform.position).normalized * speed * Time.fixedDeltaTime);
+        if((gameObject.transform.position-player.transform.position).x > 0)
+        {
+            gameObject.transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+        else
+        {
+            gameObject.transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
     }
 }
