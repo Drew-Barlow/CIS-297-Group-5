f27a73df29ce169de8f5d296a5448653ea6130e8
pesmarkd <devin.pesmark@gmail.com>
2022-04-12 18:34:51 -0400

    Enemy_Attacking

--- Treasure Hoarder/Assets/Scripts/EnemyMovement.cs
+++ Treasure Hoarder/Assets/Scripts/EnemyMovement.cs
@@ -6,15 +6,19 @@ public class EnemyMovement : MonoBehaviour
 {
 
     public Rigidbody2D rb;
+    public Animator anim;
     public Collider2D col;
     private Collider2D colPlayer;
     public GameObject player;
     public float speed;
 
+    private bool attacking;
+
     private void Start()
     {
         player = GameObject.FindWithTag("Player");
         colPlayer = player.GetComponent<Collider2D>();
+        attacking = false;
     }
 
     void Update()
@@ -22,21 +26,36 @@ public class EnemyMovement : MonoBehaviour
         //Debug.Log(gameObject.transform.position);
         if (col.IsTouching(colPlayer))
         {
+            attacking = true;
+
             rb.velocity = new Vector2(0, 0);
+            anim.Play("Skeleton_Attacking");
+            
+            //anim.SetBool("Contact", true);
+        }
+        
+        if(anim.GetCurrentAnimatorStateInfo(0).IsName("Skeleton Walking"))
+        {
+            attacking = false;
+            //anim.SetBool("Contact", false);
         }
         
     }
 
     private void FixedUpdate()
     {
-        gameObject.transform.position = gameObject.transform.position + ((player.transform.position - gameObject.transform.position).normalized * speed * Time.fixedDeltaTime);
-        if((gameObject.transform.position-player.transform.position).x > 0)
+        if (attacking == false)
         {
-            gameObject.transform.localRotation = Quaternion.Euler(0, 180, 0);
+            gameObject.transform.position = gameObject.transform.position + ((player.transform.position - gameObject.transform.position).normalized * speed * Time.fixedDeltaTime);
+            if ((gameObject.transform.position - player.transform.position).x > 0)
+            {
+                gameObject.transform.localRotation = Quaternion.Euler(0, 180, 0);
+            }
+            else
+            {
+                gameObject.transform.localRotation = Quaternion.Euler(0, 0, 0);
+            }
         }
-        else
-        {
-            gameObject.transform.localRotation = Quaternion.Euler(0, 0, 0);
-        }
+        
     }
 }
