3632d9435dd46d98efb4ea649a7f78e9279d79b9
doom-mi <dominicver@hotmail.com>
2022-04-12 00:29:00 -0400

    Created Speed Power Up
    Created speed power up that is now able to increase player speed when dropped

---
+++ Treasure Hoarder/Assets/PowerUp.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public float duration = 10;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            StartCoroutine(Pickup(collision));
+        }
+    }
+
+    private IEnumerator Pickup(Collider2D player)
+    {
+        BasicMovement speed = player.GetComponent<BasicMovement>();
+        speed.moveSpeed += 3;
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<Collider2D>().enabled = false;
+        yield return new WaitForSeconds(duration);
+        speed.moveSpeed -= 3;
+        Destroy(gameObject);
+    }
+}
+
