8656e517aa62ecc285f7019a65f7e95c41bdf4f6
Kirk Daniel Caponpon <caponponkirk24@gmail.com>
2022-04-14 15:14:43 -0400

    added character follow scene

--- Treasure Hoarder/Assets/Scripts/PlayerMovement.cs
+++ Treasure Hoarder/Assets/Scripts/PlayerMovement.cs
@@ -14,6 +14,7 @@ public class PlayerMovement : MonoBehaviour
     private float timeStamp = 1f;
     public float cooldownperiod = 1f;
     private bool Dashing;
+    
 
 
     //shooting variables 
