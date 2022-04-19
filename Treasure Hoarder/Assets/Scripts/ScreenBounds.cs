7c71a58f1cc47b3328254c8fb13d364749c0bc63
doom-mi <dominicver@hotmail.com>
2022-04-19 00:18:43 -0400

    Updated ScreenBounds Script
    AN ACTUAL SCREENBOUND THAT WORKS

--- Treasure Hoarder/Assets/Scripts/ScreenBounds.cs
+++ Treasure Hoarder/Assets/Scripts/ScreenBounds.cs
@@ -2,72 +2,26 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Events;
 
-[RequireComponent(typeof(BoxCollider2D))]
 public class ScreenBounds : MonoBehaviour
 {
-    public Camera mainCamera;
-    BoxCollider2D boxCollider;
+    private Vector2 screenBounds;
+    public Camera MainCamera;
+    private float objectWidth;
+    private float objectHeight;
 
-    public UnityEvent<Collider2D> ExitTriggerFired;
-
-    [SerializeField]
-    private float teleportOffset = 0.2f;
-
-    private void Awake()
+    void Start()
     {
-        this.mainCamera.transform.localScale = Vector3.one;
-        boxCollider = GetComponent<BoxCollider2D>();
-        boxCollider.isTrigger = true;
+        screenBounds = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x; //extents = size of width / 2
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y; //extents = size of height / 2
     }
 
-    private void Start()
+    void LateUpdate()
     {
-        transform.position = Vector3.zero;
-        UpdateBoundSize();
-    }
-
-    public void UpdateBoundSize()
-    {
-        float ySize = mainCamera.orthographicSize * 2;
-        Vector2 boxColliderSize = new Vector2(ySize * mainCamera.aspect, ySize);
-        boxCollider.size = boxColliderSize;
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        ExitTriggerFired?.Invoke(collision);
-    }
-
-    public bool AmIOutOfBounds(Vector3 worldPosition)
-    {
-        return
-            Mathf.Abs(worldPosition.x) > Mathf.Abs(boxCollider.bounds.min.x) ||
-            Mathf.Abs(worldPosition.y) > Mathf.Abs(boxCollider.bounds.min.y);
-    }
-
-    public Vector2 CalculateWrappedPosition(Vector2 worldPosition)
-    {
-        bool xBoundResult = Mathf.Abs(worldPosition.x) > Mathf.Abs(boxCollider.bounds.min.x);
-        bool yBoundResult = Mathf.Abs(worldPosition.y) > Mathf.Abs(boxCollider.bounds.min.y);
-        Vector2 signWorldPosition = new Vector2(Mathf.Sign(worldPosition.x), Mathf.Sign(worldPosition.y));
-
-        if (xBoundResult && yBoundResult)
-        {
-            return Vector2.Scale(worldPosition, Vector2.one * -1) + Vector2.Scale(new Vector2(teleportOffset, teleportOffset), signWorldPosition);
-        }
-        else if(xBoundResult)
-        {
-            return new Vector2(worldPosition.x * -1, worldPosition.y) + new Vector2(teleportOffset * signWorldPosition.x, teleportOffset);
-        }
-        else if(yBoundResult)
-        {
-            return new Vector2(worldPosition.x, worldPosition.y * -1) + new Vector2(teleportOffset, teleportOffset * signWorldPosition.y);
-        }
-        else
-        {
-            return worldPosition;
-        }
+        Vector3 viewPos = transform.position;
+        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x * -1 + objectWidth, screenBounds.x - objectWidth);
+        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y * -1 + objectHeight, screenBounds.y - objectHeight);
+        transform.position = viewPos;
     }
 }
